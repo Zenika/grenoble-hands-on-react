587817fe9d671c5f5fc9c74f3ae1e39e4a4bec49
Thomas Huchedé <thomas.huchede@zenika.com>
2020-11-09 21:52:56 +0100

    show data-binding and setState before practice
    this is where attendees will start

--- src/pages/city/City.jsx
+++ src/pages/city/City.jsx
@@ -21,12 +21,16 @@ class City extends React.Component {
                     min: 7,
                 },
                 wind10m_max: 2 
-            }
+            },
+            cityName: ''
         };
     }
 
     componentDidMount() {
         // STEP 0 : affichage des données (data-binding) et cycle de vie (lifecycle)
+        this.setState({
+            cityName: this.props.params.cityName,
+        })
 
         // STEP 1 : Utiliser la méthode getCityTodayWeather de l'objet WeatherAPI (déjà importé) pour récuperer la météo
 
@@ -37,9 +41,18 @@ class City extends React.Component {
         return <>
             <h1 className="title">Cities weather</h1>
             <article className="panel is-primary">
-            <div className="panel-heading"><h2>GRENOBLE</h2></div>
+            <div className="panel-heading"><h2>{this.state.cityName}</h2></div>
             <div className="panel-block">
                 <LMap lat={this.state.cityLatitude} long={this.state.cityLongitude}/>
+            </div>
+            <div className="panel-block">
+                <ul>
+                    <li>Date: {this.state.weather.date}</li>
+                    <li>Forecast: {this.state.weather.weather}</li>
+                    <li>Min: {this.state.weather.temp2m.min}°C</li>
+                    <li>Max: {this.state.weather.temp2m.max}°C</li>
+                    <li>Wind speed: {this.state.weather.wind10m_max}m/s</li>
+                </ul>
             </div>
             <div className="panel-block">
                 <Link to="/" className="button is-rounded">
