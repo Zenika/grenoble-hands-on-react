99525b51b6dd1c85d7e8c7e4c04216ba5f165aa8
Thomas Huchedé <thomas.huchede@zenika.com>
2020-11-10 19:16:25 +0100

    step 5

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import City from './pages/city/City';
+import AddCity from './pages/city/AddCity';
 import Home from './pages/home/Home';
 import './App.css';
 
@@ -15,9 +16,10 @@ function App() {
         <div className="container">
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="create" element={<AddCity />} />
                 <Route path=":cityName" element={<City />} />
             </Routes>
-          </div>
+        </div>
       </section>
 
       <Footer />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function NavBar() {
     return <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
@@ -12,6 +13,11 @@ function NavBar() {
             <h2 className="navbar-item has-text-white">
                 The best weather app in React
             </h2>
+            </div>
+            <div className="navbar-menu navbar-end">
+                <Link className="navbar-item" to="/create">
+                Create city
+                </Link>
             </div>
         </div>
     </nav>;
