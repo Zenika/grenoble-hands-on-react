2d068fe1cbb0e6fccb93357647ee2f927a26e25c
Elisa Lescarret <e.s.lescarret@gmail.com>
2021-01-05 20:23:21 +0100

    Step 2: Weather Api in TS

--- src/api/weather.api.ts
+++ src/api/weather.api.ts
@@ -1,15 +1,20 @@
-export function getCityNextWeekWeather(long, lat) {
+import { IDetailedWeather, IWeather } from "../types/WeatherTypes";
+
+
+export function getCityNextWeekWeather(long: number, lat: number): Promise<IWeather[]> {
     return fetch(`http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`)
     .then(response => response.json())
     .then(response => response.dataseries)
 }
-export function getCityTodayWeather(long, lat) {
+
+export function getCityTodayWeather(long: number, lat: number): Promise<IWeather> {
     return fetch(`http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`)
     .then(response => response.json())
     .then(response => response.dataseries)
     .then(dataseries => dataseries[0])
 }
-export function getDetailedCityWeather(long, lat) {
+
+export function getDetailedCityWeather(long: number, lat: number): Promise<IDetailedWeather[]> {
     return fetch(`http://www.7timer.info/bin/civil.php?lon=${long}&lat=${lat}&unit=metric&output=json`)
     .then(response => response.json())
     .then(response => response.dataseries)
--- src/pages/city/City.tsx
+++ src/pages/city/City.tsx
@@ -4,27 +4,7 @@ import { withRouter } from "../../utils/withRouter";
 import LMap from "../../components/LMap";
 import WeatherApi from "../../api/weather.api";
 import Store from "../../store/Store";
-
-interface IWeather {
-  date: number;
-  temp2m: { max: number; min: number };
-  weather: string;
-  wind10m_max: number;
-}
-
-interface IDetailedWeather {
-  lifted_index: number;
-  prec_amount: number;
-  prec_type: string;
-  rh2m: string;
-  temp2m: number;
-  timepoint: number;
-  weather: string;
-  wind10m: {
-    direction: string;
-    speed: number;
-  };
-}
+import { IDetailedWeather, IWeather } from "../../types/WeatherTypes";
 
 interface ICityProps {
   params: {
