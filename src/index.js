489ad16a8df9e55bbd5b2be2c26bf05811ffdec5
Thomas Huchedé <thomas.huchede@zenika.com>
2021-03-28 16:33:50 +0200

    mise en place node-sass pour bulma

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import 'bulma/css/bulma.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
