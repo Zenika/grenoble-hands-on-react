d8ba928d7577fc410951007074f60e9dc36dd3a6
Arno <arno.tgh@gmail.com>
2023-10-25 12:27:48 +0200

    adding vitest for testing

---
+++ src/App.test.jsx
@@ -0,0 +1,9 @@
+import { test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { App } from './App'
+
+test('renders title on navbar', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/Zenika Cities weather/i)
+  expect(linkElement).toBeInTheDocument()
+})
