cc164ee037003b83b4b614549d626c8e6dc5bf99
Arno <arno.tgh@gmail.com>
2023-10-27 15:41:19 +0200

    bonus

--- src/pages/city/CityPage.jsx
+++ src/pages/city/CityPage.jsx
@@ -1,22 +1,47 @@
 import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { LMap } from '../../components/LMap'
-import { getCityNextWeekWeather } from '../../api/weather.api'
-import { displayDate } from '../../utils/datetime.util.js'
+import {
+  getCityDetailedWeather,
+  getCityNextWeekWeather
+} from '../../api/weather.api'
+import { displayDate, displayHour } from '../../utils/datetime.util.js'
 import Store from '../../store/Store'
 import { useDegree } from '../../hooks/useDegree.js'
+import { DetailedWeatherImage } from '../../components/DetailedWeatherImage.jsx'
 import './CityPage.css'
+import '../../components/CustomLoader.css'
 
 export const CityPage = () => {
   const { cityName } = useParams()
   const { degree, setDegree, getTemperature } = useDegree()
   const { lat, long } = Store.getCityPosition(cityName)
+
+  const [isLoading, setIsLoading] = useState(false)
   const [weathers, setWeathers] = useState([])
+  const [detailedWeather, setDetailedWeather] = useState([])
+  const [mode, setMode] = useState('simple')
+
+  const fetchCityWeather = async () => {
+    try {
+      setIsLoading(true)
+      const [weathersResponseFromApi, detailedWeatherResponseFromApi] =
+        await Promise.all([
+          await getCityNextWeekWeather(long, lat),
+          await getCityDetailedWeather(long, lat),
+          await new Promise((resolve) => setTimeout(resolve, 2_000))
+        ])
+      setWeathers(weathersResponseFromApi)
+      setDetailedWeather(detailedWeatherResponseFromApi)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   useEffect(() => {
-    getCityNextWeekWeather(long, lat)
-      .then((weathers) => setWeathers(weathers))
-      .catch((err) => console.log(err))
+    fetchCityWeather()
   }, [])
 
   return (
@@ -29,51 +54,98 @@ export const CityPage = () => {
         <div className="panel-block">
           <LMap latitude={lat} longitude={long} />
         </div>
-        <div className="panel-block">
-          <div className="buttons has-addons">
-            <button
-              onClick={() => setDegree('C')}
-              className={`button ${degree === 'C' ? 'is-primary' : ''}`}
-            >
-              °C
-            </button>
-            <button
-              onClick={() => setDegree('F')}
-              className={`button ${degree === 'F' ? 'is-primary' : ''}`}
-            >
-              °F
-            </button>
-          </div>
-        </div>
-        <div className="panel-block">
-          <table className="table is-flex-grow-1">
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Weather</th>
-                <th>Min</th>
-                <th>Max</th>
-              </tr>
-            </thead>
-            <tbody>
-              {weathers.map((weather, index) => (
-                <tr key={`${weather}-${index}`}>
-                  <td>{displayDate(weather.date)}</td>
-                  <td>
-                    <img
-                      src={`http://www.7timer.info/img/misc/about_civil_${weather.weather}.png`}
-                      alt="meteo_image"
-                      className="cropped-image"
-                      width={80}
-                    />
-                  </td>
-                  <td>{getTemperature(weather.temp2m.min)}</td>
-                  <td>{getTemperature(weather.temp2m.max)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        {isLoading ? (
+          <div className="custom-loader" />
+        ) : (
+          <>
+            <div className="panel-block is-flex is-justify-content-space-between is-align-items-center">
+              <div className="buttons has-addons mt-0 mb-0">
+                <button
+                  onClick={() => setDegree('C')}
+                  className={`button ${degree === 'C' ? 'is-primary' : ''}`}
+                >
+                  °C
+                </button>
+                <button
+                  onClick={() => setDegree('F')}
+                  className={`button ${degree === 'F' ? 'is-primary' : ''}`}
+                >
+                  °F
+                </button>
+              </div>
+
+              <div className="buttons has-addons is-right mt-0 mb-0">
+                <button
+                  onClick={() => setMode('simple')}
+                  className={`button ${mode === 'simple' ? 'is-primary' : ''}`}
+                >
+                  Simple
+                </button>
+                <button
+                  onClick={() => setMode('detailed')}
+                  className={`button ${
+                    mode === 'detailed' ? 'is-primary' : ''
+                  }`}
+                >
+                  Detailed
+                </button>
+              </div>
+            </div>
+
+            {mode === 'simple' && (
+              <table className="table is-fullwidth">
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Weather</th>
+                    <th>Min</th>
+                    <th>Max</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {weathers.map((weather, index) => (
+                    <tr key={`${weather}-${index}`}>
+                      <td>{displayDate(weather.date)}</td>
+                      <td>
+                        <img
+                          src={`http://www.7timer.info/img/misc/about_civil_${weather.weather}.png`}
+                          alt="meteo_image"
+                          className="cropped-image-day"
+                          width={80}
+                        />
+                      </td>
+                      <td>{getTemperature(weather.temp2m.min)}</td>
+                      <td>{getTemperature(weather.temp2m.max)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+
+            {mode === 'detailed' && (
+              <table className="table is-fullwidth">
+                <thead>
+                  <tr>
+                    <th>Time</th>
+                    <th>Weather</th>
+                    <th>Temperature</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {detailedWeather.map((weatherPerDay, index) => (
+                    <tr key={`${weatherPerDay}-${index}`}>
+                      <td>{displayHour(weatherPerDay.timepoint)}</td>
+                      <td>
+                        <DetailedWeatherImage weather={weatherPerDay.weather} />
+                      </td>
+                      <td>{getTemperature(weatherPerDay.temp2m)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </>
+        )}
         <div className="panel-block">
           <Link to="/">
             <button className="button is-primary is-outlined">
